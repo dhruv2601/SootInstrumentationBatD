074beb9c2623edcf19259ba5f461fa064d31fe75
dhruv2601 <dhruvrathi15@gmail.com>
2018-07-16 18:59:54 -0700

    Commencing instrumentation and setting platform for Services

---
+++ src/InstrumentAPK/Constants.java
@@ -0,0 +1,36 @@
+package InstrumentAPK;
+
+class Constants {
+    final static String SCRIPTS_DIR = "/media/dhruv2601/lisp/Research/Battery_Optimization/APKcollector/Instrument/Soot_Instrumenter/InstrumentAPK/Scripts/";
+
+    // Variables for paths to Android and APKs
+    final static String ANDROID_JAR = "/home/dhruv2601/Android/Sdk/platforms/";
+    final static String APK_DIR = "/home/dhruv2601/IdeaProjects/Soot_Instrumenter/InstrumentAPK/APK/";
+    final static String OUTPUT_DIR = "/home/dhruv2601/IdeaProjects/Soot_Instrumenter/InstrumentAPK/";
+
+//    public static final String APK_NAME = "com.MobileNumberTracker.GPS.Tracking.GPSnavigation.MAPS.Location.Tracker.apk";
+//    public final static String APK_NAME = "locationshare.apk";
+//    public final static String APK_NAME = "MapDisplay.apk";
+    public final static String APK_NAME = "app-debug.apk";
+
+    //    public static final String APK_NAME = "com.PYOPYO.StarTracker.apk";
+    //    public static final String APK_NAME = "com.waze.apk";
+//    public static final String APK_NAME = "com.application.zomato";
+//    public static final String APK_NAME = "com.appmind.radios.ca";
+//    public static final String APK_NAME = "com.atlogis.camaps.free-48";
+//    public static final String APK_NAME = "com.digitalhud.speedometer";
+//    public static final String APK_NAME = "com.droid27.transparentclockweather";
+//    public static final String APK_NAME = "com.exovoid.weather.app";
+//    public static final String APK_NAME = "com.favequest.mea.cmos_congress";
+//    public static final String APK_NAME = "com.fotoable.temperature.weather";
+//    public static final String APK_NAME = "com.isrtransit.stm";
+//    public static final String APK_NAME = "com.mcdonalds.superapp";
+//    public static final String APK_NAME = "com.morkim.thunder.gps";
+//    public static final String APK_NAME = "com.passportparking.mobile.toronto";
+//    public static final String APK_NAME = "com.sygic.aura";
+//    public static final String APK_NAME = "com.tripadvisor.tripadvisor-170515015";
+//    public static final String APK_NAME = "cz.aponia.bor3.offlinemaps";
+//    public static final String APK_NAME = "me.lyft.android";
+//    public static final String APK_NAME = "org.mrchops.android.digihud";
+
+}
